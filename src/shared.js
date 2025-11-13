4f4dc5f04402cdeef3ded375a94003b90086266f
ArnT81 <anders_soderberg@hotmail.com>
2025-11-13 10:01:37 +0100

    shared validateControl restructured

--- src/shared.js
+++ src/shared.js
@@ -1,15 +1,24 @@
+function showError(el, formGroup) {
+	el.classList.add('is-invalid');
+	el.classList.remove('is-valid');
+	formGroup?.classList.add('is-invalid');
+	formGroup?.classList.remove('is-valid');
+	return true;
+};
+
+function removeError(el, formGroup) {
+	el.classList.remove('is-invalid');
+	el.classList.add('is-valid');
+	formGroup?.classList.remove('is-invalid');
+	formGroup?.classList.add('is-valid');
+	return false;
+}
+
 export function validateControl(el) {
 	if (!el) return false;
+	const formGroup = el.closest('.mb-3, fieldset, .form-check');
+	const isValid = el.checkValidity();
 
-	if (el.checkValidity()) {
-		el.classList.remove('is-invalid');
-		el.classList.add('is-valid');
-		el.removeAttribute('aria-invalid');
-		return true;
-	} else {
-		el.classList.remove('is-valid');
-		el.classList.add('is-invalid');
-		el.setAttribute('aria-invalid', 'true');
-		return false;
-	}
+	if (!isValid) return showError(el, formGroup);
+	else removeError(el, formGroup);
 };
